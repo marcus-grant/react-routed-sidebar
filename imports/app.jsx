268136f97f0dcbc49a8613c545e1cff3b32b1ed5
Marcus Grant <marcusfg@gmail.com>
2017-09-08 17:11:16 -0400

    re-creating environment with meteor

---
+++ imports/app.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+//import from './hello-world.jsx';
+
+export default class App extends Component {
+  render() {
+    return (
+      <h1>Hello World!</h1>
+    );
+  }
+}
